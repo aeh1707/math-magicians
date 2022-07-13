c64bf5c1026f2a646c82a6ccdd47c94df6b35a9c
aeh1707d <abdelhadi.hireche@outlook.com>
2022-07-13 20:34:23 +0100

    Create Calculator component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Calculator name="aeh1707" />
   </React.StrictMode>,
 );
 
