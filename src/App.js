6bb7eb869c274abef594115543d25ecdf6846ef2
aeh1707d <abdelhadi.hireche@outlook.com>
2022-07-13 20:37:18 +0100

    Add Calculator component to main App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Math Magicians</h1>
+        <Calculator />
       </header>
     </div>
   );
