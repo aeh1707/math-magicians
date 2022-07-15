9e92e50f74e9518c0c570deeb917df1c1f5539d9
aeh1707d <abdelhadi.hireche@outlook.com>
2022-07-15 01:00:22 +0100

    Refactor Calculator component with useState hook

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,47 +1,46 @@
 import './Calculator.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import Screen from './Screen';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.onButtonPress = this.onButtonPress.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: 0,
+    operation: null,
+  });
 
-  onButtonPress({ target }) {
-    this.setState((state) => calculate(state, target.innerText));
-  }
+  const onButtonPress = ({ target }) => {
+    setState((state) => calculate(state, target.innerText));
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <article className="calculator">
-        <Screen total={total} next={next} operation={operation} />
-        <Button bgColor="btn-gray" btnRef="AC" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="+/-" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="%" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-orange" btnRef="÷" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="7" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="8" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="9" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-orange" btnRef="x" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="4" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="5" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="6" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-orange" btnRef="-" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="1" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="2" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="3" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-orange" btnRef="+" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray zero" btnRef="0" handleClick={this.onButtonPress} />
-        <Button bgColor="btn-gray" btnRef="." handleClick={this.onButtonPress} />
-        <Button bgColor="btn-orange" btnRef="=" handleClick={this.onButtonPress} />
-      </article>
-    );
-  }
-}
+  const { total, next, operation } = state;
+
+  return (
+    <article className="calculator">
+      <Screen total={total} next={next} operation={operation} />
+      <Button bgColor="btn-gray" btnRef="AC" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="+/-" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="%" handleClick={onButtonPress} />
+      <Button bgColor="btn-orange" btnRef="÷" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="7" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="8" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="9" handleClick={onButtonPress} />
+      <Button bgColor="btn-orange" btnRef="x" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="4" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="5" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="6" handleClick={onButtonPress} />
+      <Button bgColor="btn-orange" btnRef="-" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="1" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="2" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="3" handleClick={onButtonPress} />
+      <Button bgColor="btn-orange" btnRef="+" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray zero" btnRef="0" handleClick={onButtonPress} />
+      <Button bgColor="btn-gray" btnRef="." handleClick={onButtonPress} />
+      <Button bgColor="btn-orange" btnRef="=" handleClick={onButtonPress} />
+    </article>
+  );
+};
 
 export default Calculator;
