564763d4872e8052474aaf57ee4a07e4eae48b1e
aeh1707d <abdelhadi.hireche@outlook.com>
2022-07-20 14:27:10 +0100

    Add operate tests

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,27 +1,27 @@
-import calculate from './src/logic/calculate';
+import calculate from './calculate';
 
 it('check if obj.next to equal 5', () => {
-    const btnPressed = "5";
-    const obj = {
-      total: "10",
-      next: "0",
-      operation: '+',
-    }
+  const btnPressed = '5';
+  const obj = {
+    total: '10',
+    next: '0',
+    operation: '+',
+  };
 
-    expect(calculate(obj, btnPressed)).toEqual({
-      total: "10",
-      next: "5",
-      operation: '+',
-    })
-  })
+  expect(calculate(obj, btnPressed)).toEqual({
+    total: '10',
+    next: '5',
+    operation: '+',
+  });
+});
 
-  it('check if returned obj is empty, when total contains period', () => {
-      const btnPressed = ".";
-      const obj = {
-        total: "0.34",
-        next: null,
-        operation: null,
-      }
+it('check if returned obj is empty, when total contains period', () => {
+  const btnPressed = '.';
+  const obj = {
+    total: '0.34',
+    next: null,
+    operation: null,
+  };
 
-      expect(calculate(obj, btnPressed)).toEqual({});
-    })
+  expect(calculate(obj, btnPressed)).toEqual({});
+});
