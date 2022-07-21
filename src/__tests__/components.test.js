4377e057d4ca61820f999ec2a0b2d9ad21518f12
petushka1 <nadya@devery.io>
2022-07-21 10:23:18 +0300

    add additional components tests

--- src/__tests__/components.test.js
+++ src/__tests__/components.test.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../components/Header';
 import Home from '../components/Home';
 import Quote from '../components/Quote';
+import Calculator from '../components/Calculator';
+import Button from '../components/Button';
+import Screen from '../components/Screen';
 
 describe('Header renders correctly', () => {
   test('Renders Header Correctly', () => {
@@ -24,5 +27,26 @@ describe('Quote renders correctly', () => {
   test('Renders Header Correctly', () => {
     const quote = render(<Router><Quote /></Router>);
     expect(quote).toMatchSnapshot();
+  });
+});
+
+describe('Calculator renders correctly', () => {
+  test('Renders Calculator Correctly', () => {
+    const calc = render(<Router><Calculator /></Router>);
+    expect(calc).toMatchSnapshot();
+  });
+});
+
+describe('Button renders correctly', () => {
+  test('Renders Button Correctly', () => {
+    const btn = render(<Router><Button /></Router>);
+    expect(btn).toMatchSnapshot();
+  });
+});
+
+describe('Screen renders correctly', () => {
+  test('Renders Screen Correctly', () => {
+    const scr = render(<Router><Screen /></Router>);
+    expect(scr).toMatchSnapshot();
   });
 });
